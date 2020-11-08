614c958645b55a2faf27f576f9036de293291704
yas78 <aug.nydi@gmail.com>
2020-11-08 20:49:20 +0900

    リファクタリング

---
+++ QRCodeLib/Image/Graphics.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using Ys.QRCode;
+
+namespace Ys.Image
+{
+    internal class GraphicPath
+    {
+        public static Point[][] FindContours(int[][] image)
+        {
+            var paths = new List<Point[]>();
+
+            for (int y = 0; y < image.Length - 1; ++y)
+            {
+                for (int x = 0; x < image[y].Length - 1; ++x)
+                {
+                    if (image[y][x] == int.MaxValue)
+                        continue;
+
+                    if (!(image[y][x] > 0 && image[y][x + 1] <= 0))
+                        continue;
+
+                    image[y][x] = int.MaxValue;
+                    var start = new Point(x, y);
+                    var path = new List<Point> { start };
+
+                    Direction dr = Direction.UP;
+                    Point p = new Point(start.X, start.Y - 1);
+
+                    do
+                    {
+                        switch (dr)
+                        {
+                            case Direction.UP:
+                                if (image[p.Y][p.X] > 0)
+                                {
+                                    image[p.Y][p.X] = int.MaxValue;
+
+                                    if (image[p.Y][p.X + 1] <= 0)
+                                        p = new Point(p.X, p.Y - 1);
+                                    else
+                                    {
+                                        path.Add(p);
+                                        dr = Direction.RIGHT;
+                                        p = new Point(p.X + 1, p.Y);
+                                    }
+                                }
+                                else
+                                {
+                                    p = new Point(p.X, p.Y + 1);
+                                    path.Add(p);
+                                    dr = Direction.LEFT;
+                                    p = new Point(p.X - 1, p.Y);
+                                }
+                                break;
+
+                            case Direction.DOWN:
+                                if (image[p.Y][p.X] > 0)
+                                {
+                                    image[p.Y][p.X] = int.MaxValue;
+
+                                    if (image[p.Y][p.X - 1] <= 0)
+                                        p = new Point(p.X, p.Y + 1);
+                                    else
+                                    {
+                                        path.Add(p);
+                                        dr = Direction.LEFT;
+                                        p = new Point(p.X - 1, p.Y);
+                                    }
+                                }
+                                else
+                                {
+                                    p = new Point(p.X, p.Y - 1);
+                                    path.Add(p);
+                                    dr = Direction.RIGHT;
+                                    p = new Point(p.X + 1, p.Y);
+                                }
+                                break;
+
+                            case Direction.LEFT:
+                                if (image[p.Y][p.X] > 0)
+                                {
+                                    image[p.Y][p.X] = int.MaxValue;
+
+                                    if (image[p.Y - 1][p.X] <= 0)
+                                        p = new Point(p.X - 1, p.Y);
+                                    else
+                                    {
+                                        path.Add(p);
+                                        dr = Direction.UP;
+                                        p = new Point(p.X, p.Y - 1);
+                                    }
+                                }
+                                else
+                                {
+                                    p = new Point(p.X + 1, p.Y);
+                                    path.Add(p);
+                                    dr = Direction.DOWN;
+                                    p = new Point(p.X, p.Y + 1);
+                                }
+                                break;
+
+                            case Direction.RIGHT:
+                                if (image[p.Y][p.X] > 0)
+                                {
+                                    image[p.Y][p.X] = int.MaxValue;
+
+                                    if (image[p.Y + 1][p.X] <= 0)
+                                        p = new Point(p.X + 1, p.Y);
+                                    else
+                                    {
+                                        path.Add(p);
+                                        dr = Direction.DOWN;
+                                        p = new Point(p.X, p.Y + 1);
+                                    }
+                                }
+                                else
+                                {
+                                    p = new Point(p.X - 1, p.Y);
+                                    path.Add(p);
+                                    dr = Direction.UP;
+                                    p = new Point(p.X, p.Y - 1);
+                                }
+                                break;
+
+                            default:
+                                throw new InvalidOperationException();
+                        }
+
+                    } while (p != start);
+
+                    paths.Add(path.ToArray());
+                }
+            }
+
+            return paths.ToArray();
+        }
+    }
+}
